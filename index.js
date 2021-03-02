074b53af0632e08360d482c78f06f621e8519bb0
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2021-03-02 15:49:09 +0300

    Add X-Response-Time header

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const Koa = require('koa');
 
 const app = new Koa();
-const arr = new Array(1e6);
+
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url}`);
+});
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+
+})
 
 app.use(async ctx => {
     ctx.body = '<h1>Hello, World!</h1>';
