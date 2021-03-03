c8c1b2c2fb95fa9cd209ce766fa77487dc818b58
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2021-03-03 18:07:00 +0300

    Add PM2. Generated ecosystem.config.js

---
+++ ecosystem.config/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000
+    },
+    env_prod: {
+      NODE_ENV: 'production',
+      PORT: 80
+    }
+  }]
+};
