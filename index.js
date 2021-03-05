6dde8ab22e28b5fc3bd256510a4129e2e927d411
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-05 14:39:21 +0300

    add routes

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  await next();
-  console.log(`${ctx.method} ${ctx.url}`);
+router.get("/", (ctx, next) => {
+  ctx.body = "<h1>Home GET</h1>";
+  next();
 });
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set("X-Response-Time", `${ms}ms`);
-});
-
-app.use(async (ctx) => {
-  ctx.body = '<h1 style="color: green;">Hello, World!</h1>';
-  if (ctx.method === "GET" && ctx.url === "/about") {
-    ctx.body = "<h1>About</h1>";
-  }
-});
-
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(process.env.PORT, () => {
   console.log("Server has been running...");
 });
