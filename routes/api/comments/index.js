55ac7efd2ce7623cebfa164df6f78f3233313997
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-10 13:37:21 +0300

    add functions for routes

--- routes/api/comments/index.js
+++ routes/api/comments/index.js
@@ -3,25 +3,31 @@ const Router = require("koa-router");
 const comments = new Router();
 
 comments
-  .get("/comments/post/:id", (ctx, next) => {
-    ctx.body = "Get all comments by post";
-    next();
-  })
-  .get("/comment/:id", (ctx, next) => {
-    ctx.body = "Get one comment by ID";
-    next();
-  })
-  .post("/comment", (ctx, next) => {
-    ctx.body = "Create a new comment";
-    next();
-  })
-  .put("/comment/:id", (ctx, next) => {
-    ctx.body = "Update a comment";
-    next();
-  })
-  .delete("/comment/:id", (ctx, next) => {
-    ctx.body = "Delete a comment";
-    next();
-  });
+  .get("/comments/post/:id", getAllComments)
+  .get("/comment/:id", getCommentById)
+  .post("/comment", createComment)
+  .put("/comment/:id", updateComment)
+  .delete("/comment/:id", removeComment);
+
+function getAllComments(ctx, next) {
+  ctx.body = "Get all comments by post";
+  next();
+}
+function getCommentById(ctx, next) {
+  ctx.body = "Get one comment by ID";
+  next();
+}
+function createComment(ctx, next) {
+  ctx.body = "Create a new comment";
+  next();
+}
+function updateComment(ctx, next) {
+  ctx.body = "Update a comment";
+  next();
+}
+function removeComment(ctx, next) {
+  ctx.body = "Delete a comment";
+  next();
+}
 
 module.exports = comments;
