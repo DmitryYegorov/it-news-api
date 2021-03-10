a8b56cd9a8f413d9c781666ddf5a8d65aef9e6ff
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-10 15:04:34 +0300

    add status codes for comments requests

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -13,36 +13,43 @@ posts
 
 function getAllPosts(ctx, next) {
   ctx.body = "Get the posts by author (userId)";
+  ctx.status = 200;
   next();
 }
 
 function getPostById(ctx, next) {
   ctx.body = "Get the posts by category";
+  ctx.status = 200;
   next();
 }
 
 function getPostsByAuthor(ctx, next) {
   ctx.body = "Get the posts by author (userId)";
+  ctx.status = 200;
   next();
 }
 
 function getPostsByCategory(ctx, next) {
   ctx.body = "Get the posts by category";
+  ctx.status = 200;
   next();
 }
 
 function createPost(ctx, next) {
   ctx.body = "Create a new post";
+  ctx.status = 201;
   next();
 }
 
 function updatePost(ctx, next) {
   ctx.body = "Update a post";
+  ctx.status = 200;
   next();
 }
 
 function removePost(ctx, next) {
   ctx.body = "Delete a post";
+  ctx.status = 204;
   next();
 }
 
