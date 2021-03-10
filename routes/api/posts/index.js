399a14aac61865ce78f2fc98077a5925da3d2727
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-10 14:46:24 +0300

    moving handlers into a separate function

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -3,33 +3,47 @@ const Router = require("koa-router");
 const posts = new Router();
 
 posts
-  .get("/posts", (ctx, next) => {
-    ctx.body = "Get all posts";
-    next();
-  })
-  .get("/post/:id", (ctx, next) => {
-    ctx.body = "Get one post by ID";
-    next();
-  })
-  .get("/posts/author/:id", (ctx, next) => {
-    ctx.body = "Get the posts by author (userId)";
-    next();
-  })
-  .get("/posts/category/:id", (ctx, next) => {
-    ctx.body = "Get the posts by category";
-    next();
-  })
-  .post("/post", (ctx, next) => {
-    ctx.body = "Create a new post";
-    next();
-  })
-  .put("/post/:id", (ctx, next) => {
-    ctx.body = "Update a new post";
-    next();
-  })
-  .delete("/post/:id", (ctx, next) => {
-    ctx.body = "Delete a post";
-    next();
-  });
+  .get("/posts", getAllPosts)
+  .get("/post/:id", getPostById)
+  .get("/posts/author/:id", getPostsByCategory)
+  .get("/posts/category/:id", getPostsByAuthor)
+  .post("/post", createPost)
+  .put("/post/:id", updatePost)
+  .delete("/post/:id", removePost);
+
+function getAllPosts(ctx, next) {
+  ctx.body = "Get the posts by author (userId)";
+  next();
+}
+
+function getPostById(ctx, next) {
+  ctx.body = "Get the posts by category";
+  next();
+}
+
+function getPostsByAuthor(ctx, next) {
+  ctx.body = "Get the posts by author (userId)";
+  next();
+}
+
+function getPostsByCategory(ctx, next) {
+  ctx.body = "Get the posts by category";
+  next();
+}
+
+function createPost(ctx, next) {
+  ctx.body = "Create a new post";
+  next();
+}
+
+function updatePost(ctx, next) {
+  ctx.body = "Update a post";
+  next();
+}
+
+function removePost(ctx, next) {
+  ctx.body = "Delete a post";
+  next();
+}
 
 module.exports = posts;
