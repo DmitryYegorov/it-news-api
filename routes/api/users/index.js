272fd8d463c5779742052ae144adca55662fe88b
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-10 13:51:22 +0300

    add middleware functions to users route

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,25 +3,31 @@ const Router = require("koa-router");
 const users = new Router();
 
 users
-  .get("/users", (ctx, next) => {
-    ctx.body = "Get all users";
-    next();
-  })
-  .get("/users/:id", (ctx, next) => {
-    ctx.body = "Get one user by ID";
-    next();
-  })
-  .post("/user", (ctx, next) => {
-    ctx.body = "Create a new users";
-    next();
-  })
-  .put("/user/:id", (ctx, next) => {
-    ctx.body = "Update a users";
-    next();
-  })
-  .delete("/user/:id", (ctx, next) => {
-    ctx.body = "Delete a users";
-    next();
-  });
+  .get("/users", getAllUsers)
+  .get("/users/:id", getUserById)
+  .post("/user", createUser)
+  .put("/user/:id", updateUser)
+  .delete("/user/:id", removeUser);
+
+function getAllUsers(ctx, next) {
+  ctx.body = "Get all users";
+  next();
+}
+function getUserById(ctx, next) {
+  ctx.body = "Get one user by ID";
+  next();
+}
+function createUser(ctx, next) {
+  ctx.body = "Create a new user";
+  next();
+}
+function updateUser(ctx, next) {
+  ctx.body = "Update a users";
+  next();
+}
+function removeUser(ctx, next) {
+  ctx.body = "Delete a users";
+  next();
+}
 
 module.exports = users;
