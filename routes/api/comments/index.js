29cc6d4dc2414804f967a3127ac514e83abbbb83
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-10 14:49:38 +0300

    add empty lines between function declarations

--- routes/api/comments/index.js
+++ routes/api/comments/index.js
@@ -13,18 +13,22 @@ function getAllComments(ctx, next) {
   ctx.body = "Get all comments by post";
   next();
 }
+
 function getCommentById(ctx, next) {
   ctx.body = "Get one comment by ID";
   next();
 }
+
 function createComment(ctx, next) {
   ctx.body = "Create a new comment";
   next();
 }
+
 function updateComment(ctx, next) {
   ctx.body = "Update a comment";
   next();
 }
+
 function removeComment(ctx, next) {
   ctx.body = "Delete a comment";
   next();
