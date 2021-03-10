fe68b8b5fbce64dcc1dde2b5e7468ad3de57c9b0
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-10 17:29:31 +0300

    add middlewares koa-json, @koa/cors, koa-logger, koa-bodyparser

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = new Koa();
 const { PORT } = process.env;
 const api = require("./routes/api");
 
-app.use(koaJson);
-app.use(koaBodyParser);
-app.use(koaCors);
-app.use(koaLogger);
+app.use(koaJson());
+app.use(koaBodyParser());
+app.use(koaCors());
+app.use(koaLogger());
 app.use(api.routes());
 app.use(api.allowedMethods());
 app.listen(PORT);
