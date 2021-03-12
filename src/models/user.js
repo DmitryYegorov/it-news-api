ae0f52185c2988982d051e09955705d8863f33ae
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-12 14:36:51 +0300

    add jsonSchema(), getters tableName and idColumn

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,24 @@ class UserModel extends Model {
   static get tableName() {
     return "users";
   }
+
+  static get idColumn() {
+    return "userid";
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["userid", "userlogin", "userlogin", "useremail"],
+
+      properties: {
+        userid: { type: "integer" },
+        userlogin: { type: "string", minLength: 5, maxLength: 60 },
+        username: { type: "string", minLength: 10, maxLength: 255 },
+        userpassword: { type: "string", minLength: 8, maxLength: 255 },
+      },
+    };
+  }
 }
 
 module.exports = UserModel;
