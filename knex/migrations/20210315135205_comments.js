9f15317b0cb89a90da71174240321d34089b28c4
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-15 16:56:46 +0300

    create migrations for Comments table

---
+++ knex/migrations/20210315135205_comments.js
@@ -0,0 +1,24 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("comments", function (table) {
+    table.increments("commentid").primary().notNullable();
+    table
+      .integer("commentauthor")
+      .index()
+      .references("userid")
+      .inTable("users")
+      .onDelete("SET NULL");
+    table.text("commenttext").notNullable();
+    table
+      .integer("commentpost")
+      .index()
+      .references("postid")
+      .inTable("posts")
+      .onDelete("SET NULL");
+    table.dateTime("created_at").notNullable();
+    table.dateTime("updated_at").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("comments");
+};
