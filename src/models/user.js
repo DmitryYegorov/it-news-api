f6b49fbe5dd8e04241195a8eedb20c89bf221f68
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2021-03-16 16:14:44 +0300

    hide password with objection-visibile

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,11 @@
+const { visibilityPlugin } = require("objection-visibility");
 const Model = require("./BaseModel");
 
-class UserModel extends Model {
+class UserModel extends visibilityPlugin(Model) {
+  static get hidden() {
+    return ["password"];
+  }
+
   static get tableName() {
     return "users";
   }
@@ -16,11 +21,10 @@ class UserModel extends Model {
   static get jsonSchema() {
     return {
       type: "object",
-      required: ["name", "login", "email"],
+      required: ["name", "email"],
 
       properties: {
         id: { type: "integer" },
-        login: { type: "string", minLength: 5, maxLength: 60 },
         email: { type: "string", minLength: 10, maxLength: 60 },
         name: { type: "string", minLength: 10, maxLength: 255 },
         password: { type: "string", minLength: 8, maxLength: 255 },
